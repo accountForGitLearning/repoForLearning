23f8f20d452cafcdb46fcf103ed768f28b6bc244
n.kibitkin <@n.kibitkin>
2020-12-05 16:22:43 +0700

    A

---
+++ src/com/test/Main.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import com.test.printers.APrinter;
+import com.test.printers.Printer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Printer printer = new APrinter();
+
+        printer.print();
+    }
+}
---
+++ src/com/test/printers/APrinter.java
@@ -0,0 +1,8 @@
+package com.test.printers;
+
+public class APrinter implements Printer {
+
+    public void print(){
+        System.out.println("This is 'A' printer");
+    }
+}
